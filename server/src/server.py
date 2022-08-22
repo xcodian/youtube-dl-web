d7f76fd002b4b5942ec8c98d09d937c863623bbd
Codian <44289933+xxcodianxx@users.noreply.github.com>
2022-08-22 13:42:37 +0930

    feat(dl): properly guess the file extension of downloaded files with libmagic

--- server/src/server.py
+++ server/src/server.py
@@ -1,10 +1,14 @@
+import mimetypes
 import os
-from fastapi import FastAPI, Response, HTTPException
+
+import magic
+
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse, JSONResponse, FileResponse
 from fastapi.background import BackgroundTasks
+
 from util.sub import download_subs
 from util.meta import query_meta
-
 from util.stream import stream_from_yt
 
 app = FastAPI()
@@ -15,22 +19,35 @@ async def api_dl(
     f: str = "best", # format 
     sl: str = None,  # subtitle language to embed
 ):
-    if "+" in f or sl is not None:
-        # video will have to be remuxed into matroska
-        media_type = "video/mkv"
-        headers = {
-            "Content-Disposition": f"attachment;filename={video_id}.mkv"
-        }
-    else:
-        # video can be sent as-is
-        media_type = None
-        headers = None
+    stream = stream_from_yt(video_id, f, sl)  
+    first_chunk = await stream.__anext__() # peek first chunk
 
+    # guess filetype
+    m = magic.Magic(mime=True, uncompress=True)
+    mime_type = m.from_buffer(first_chunk)
+
+    # guess extension based on mimetype
+    ext = mimetypes.guess_extension(mime_type) or '.mkv' # fallback to mkv I guess
+    
+    print(mime_type, ext)
+    
+    headers = {
+        "Content-Disposition": f"attachment;filename={video_id}{ext}"
+    }
+
+    async def joined_stream():
+        # attach the first chunk back to the generator
+        yield first_chunk
+
+        # pass on the rest
+        async for chunk in stream:
+            yield chunk
+
+    # pass that to the user
     return StreamingResponse(
-        stream_from_yt(video_id, f, sl),
-        # if remuxed, specify matroska, otherwise just let it guess filetype
-        media_type = media_type,
-        headers=headers
+        joined_stream(),
+        media_type = mime_type,
+        headers = headers
     )
 
 @app.get("/meta/{video_id}")
